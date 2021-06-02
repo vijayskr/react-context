079df70337043fc98bf4e5d69eeafc1f8a24032a
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2021-06-01 20:36:13 -0400

    Updated REDUX with ReactContext API - Just a quick learning
    Updated REDUX with ReactContext API - Just a quick learning

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext }  from 'react';
+//import { useSelector } from 'react-redux';
+import { ProductsContext } from '../context/products-context';
 
 import FavoriteItem from '../components/Favorites/FavoriteItem';
 import './Products.css';
 
 const Favorites = props => {
+  const favoriteProducts = useContext(ProductsContext).products.filter(p => p.isFavorite);
+
+  /*
   const favoriteProducts = useSelector(state =>
     state.shop.products.filter(p => p.isFavorite)
   );
+  */
+ 
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+//import { Provider } from 'react-redux';
+//import { combineReducers, createStore } from 'redux';
+import ProductsProvider from "./context/products-context.js";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import { BrowserRouter } from "react-router-dom";
 
+import "./index.css";
+import App from "./App";
+//import productReducer from "./store/reducers/products";
+
+//Replacing REDUX with reactHooks ==> New learnings...
+
+/*
 const rootReducer = combineReducers({
   shop: productReducer
 });
 
 const store = createStore(rootReducer);
+*/
 
-ReactDOM.render(
+/*ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
+  */
+
+ReactDOM.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  document.getElementById("root")
 );
